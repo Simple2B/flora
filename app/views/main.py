810f6c21fdce4bb653b9e82b73eccba1b5bdad97
Vitaly <dagger180@gmail.com>
2020-09-15 18:08:34 +0300

    customize_flash_message

--- app/views/main.py
+++ app/views/main.py
@@ -1,8 +1,20 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, redirect, url_for
 
 main_blueprint = Blueprint('main', __name__)
 
 
 @main_blueprint.route('/')
 def index():
+    return redirect(url_for('main.team'))
+
+@main_blueprint.route('/bidding')
+def bidding():
+    return render_template('index.html')
+
+@main_blueprint.route('/team')
+def team():
+    return render_template('index.html')
+
+@main_blueprint.route('/resources')
+def resources():
     return render_template('index.html')
