0c8b509b8b09f56f3f2833d4e0a19e030c710efe
Vitaly <dagger180@gmail.com>
2020-09-16 15:11:40 +0300

    creating_team_page

--- app/views/main.py
+++ app/views/main.py
@@ -1,4 +1,5 @@
-from flask import render_template, Blueprint, redirect, url_for
+from flask import render_template, Blueprint, redirect, url_for, request
+from app.forms import LoginForm
 
 main_blueprint = Blueprint('main', __name__)
 
@@ -7,13 +8,18 @@ main_blueprint = Blueprint('main', __name__)
 def index():
     return redirect(url_for('main.team'))
 
+
 @main_blueprint.route('/bidding')
 def bidding():
     return render_template('index.html')
 
+
 @main_blueprint.route('/team')
 def team():
-    return render_template('team.html')
+    form = LoginForm(request.form)
+    form.user = 'Administrator'
+    return render_template('team.html', form=form)
+
 
 @main_blueprint.route('/resources')
 def resources():
