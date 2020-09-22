9d74a75bf831a4313fc07ce9b2ca63063002ab2d
Vitaly <dagger180@gmail.com>
2020-09-22 16:51:29 +0300

    add_tabs_to_bidding_page

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -10,6 +10,25 @@
     //     // $(this).addClass('ddb-underline');
     // });
     // // });
+
+function changeTab(evt, cityName) {
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+//
 const indicator = document.querySelector('.nav-indicator');
 const items = document.querySelectorAll('.menu__item');
 
