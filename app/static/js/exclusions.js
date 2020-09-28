ae2f06e1487c295380712d62b69b3d7bb7a8ce56
Vitaly <dagger180@gmail.com>
2020-09-28 19:01:54 +0300

    fix_layout_create_modal-delete-exclusion

--- app/static/js/exclusions.js
+++ app/static/js/exclusions.js
@@ -19,5 +19,16 @@ $(document).ready(function() {
       modal.find('.modal-body form').attr('action', target_link);
       modal.find('.modal-body #title').val(title);
       modal.find('.modal-body #description').val(description);
+
     });
+
+    $('#modalDelete').on('show.bs.modal', function (event) {
+        const button = $(event.relatedTarget); // Button that triggered the modal
+        const target_link = button.data('target_link_delete');
+        const title = button.data('delete');
+        const modal = $(this);
+        modal.find('.modal-body #title').val(title);
+        modal.find('#delete_item').attr('action', target_link);
+    });
+
 } );
--- app/views/exclusion.py
+++ app/views/exclusion.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, url_for, redirect, flash, request, session
+from flask import Blueprint, render_template, url_for, redirect, request, session
 from flask_login import login_required
 
 from app.models import Exclusion
@@ -69,6 +69,21 @@ def delete_exclusion_item_from_cart(item_id):
     return redirect(url_for("exclusion.exclusions"))
 
 
+@exclusion_blueprint.route(
+    "/delete_exclusion_item_from_items/<item_id>", methods=["POST"]
+)
+@login_required
+def delete_exclusion_item_from_items(item_id):
+    exclusion = Exclusion.query.get(item_id)
+    if exclusion:
+        selected = session.get("SelectedExclusionItemsDict", {})
+        if str(exclusion.id) in selected:
+            del selected[str(exclusion.id)]
+            session['SelectedExclusionItemsDict'] = selected
+        exclusion.delete()
+    return redirect(url_for("exclusion.exclusions"))
+
+
 @exclusion_blueprint.route("/edit_exclusion_item/<item_id>", methods=["POST"])
 @login_required
 def edit_exclusion_item(item_id):
@@ -78,35 +93,8 @@ def edit_exclusion_item(item_id):
         exclusion = Exclusion.query.get(item_id)
         if exclusion:
             exclusion.title = form.title.data
-            exclusion.description = form.description.data
             exclusion.save()
     return redirect(url_for("exclusion.exclusions"))
-
-
-# @exclusion_blueprint.route("/add_exclusion_to_cart", methods=["POST"])
-# @login_required
-# def add_exclusion_to_cart():
-#     form = ExclusionCartForm(request.form)
-#     selected_ids = session.get("SelectedExclusionItemsDict", {})
-#     form.selected_exclusion_items = {
-#         int(item_id): Exclusion.query.get(item_id) for item_id in selected_ids
-#     }
-#     if form.validate_on_submit():
-#         form.selected_exclusion_items.update(
-#             {
-#                 str(k): Exclusion.query.get(int(k))
-#                 for k in request.form
-#                 if request.form[k] == "on"
-#             }
-#         )
-#         session["SelectedExclusionItemsDict"] = {
-#             str(item_id): item_id for item_id in form.selected_exclusion_items
-#         }
-#         return redirect(url_for("exclusion.exclusions"))
-#     elif form.is_submitted():
-#         pass
-#         # flash("The given data was invalid.", "danger")
-#     return redirect(url_for("exclusion.exclusions"))
 
 
 @exclusion_blueprint.route("/exclusions", methods=["GET"])
@@ -116,9 +104,17 @@ def exclusions():
     form.exclusions = Exclusion.query.all()
     exclusion_cart_form = ExclusionCartForm()
     selected_exclusion_item_ids = session.get("SelectedExclusionItemsDict", {})
-    exclusion_cart_form.selected_work_items = [
+    exclusion_cart_form.selected_exclusions = [
         Exclusion.query.get(item_id) for item_id in selected_exclusion_item_ids
     ]
+    exclusion_cart_form.result_text = ""
+    for item in exclusion_cart_form.selected_exclusions:
+        if not exclusion_cart_form.result_text:
+            exclusion_cart_form.result_text = item.title
+        else:
+            exclusion_cart_form.result_text += ','
+            exclusion_cart_form.result_text += item.title
+
     return render_template(
         "exclusions.html",
         form=form,
