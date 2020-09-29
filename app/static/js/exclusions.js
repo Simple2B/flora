f207e1c521754960c501ef9c5b05a3623d3468a6
Vitaly <dagger180@gmail.com>
2020-09-29 10:32:48 +0300

    add_modal_tp_page_clarification

--- app/static/js/exclusions.js
+++ app/static/js/exclusions.js
@@ -1,15 +1,4 @@
 $(document).ready(function() {
-    $('#exclusionsTable').DataTable({
-        "pageLength": 10,
-        "order": [],
-        "displayStart": 0
-    });
-    $('#selectedExclusionTable').DataTable({
-        "pageLength": 10,
-        "order": [],
-        "displayStart": 0
-    });
-
     $('#exampleModal').on('show.bs.modal', function (event) {
       const button = $(event.relatedTarget); // Button that triggered the modal
       const target_link = button.data('target_link');
@@ -30,5 +19,19 @@ $(document).ready(function() {
         modal.find('#_exclusion_input_delete #title').val(title);
         modal.find('#delete_item').attr('action', target_link);
     });
+
+    $('#exclusionsTable').DataTable({
+        "pageLength": 10,
+        "order": [],
+        "displayStart": 0,
+        "drawCallback": function( settings ) {
+            $("#exclusionsTable thead").remove();
+        }
+    });
+    // $('#selectedExclusionTable').DataTable({
+    //     "pageLength": 10,
+    //     "order": [],
+    //     "displayStart": 0
+    // });
 
 } );
