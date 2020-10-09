056336d28fa0b9edc5ccb7ac29f2ae806e95d2db
Vitaly <dagger180@gmail.com>
2020-10-09 07:58:45 +0300

    fix_bid_routes_on_work_item_page

--- app/views/work_item.py
+++ app/views/work_item.py
@@ -9,9 +9,9 @@ from app.logger import log
 work_item_blueprint = Blueprint("work_item", __name__)
 
 
-@work_item_blueprint.route("/work_item", methods=["POST"])
+@work_item_blueprint.route("/work_item/<bid_id>", methods=["POST"])
 @login_required
-def work_item():
+def work_item(bid_id):
     form = NewWorkItemForm(request.form)
     if form.validate_on_submit():
         if add_work_item_validator(form.code.data):
@@ -21,19 +21,19 @@ def work_item():
             )
             work_item.save()
             # flash("Registration successful. You are logged in.", "success")
-            return redirect(url_for("work_item.work_items"))
+            return redirect(url_for("work_item.work_items", bid_id=bid_id))
         else:
             pass
             # flash("The given data was invalid.", "danger")
-            return redirect(url_for("work_item.work_items"))
+            return redirect(url_for("work_item.work_items", bid_id=bid_id))
     elif form.is_submitted():
         flash("The given data was invalid.", "danger")
-    return redirect(url_for("work_item.work_items"))
+    return redirect(url_for("work_item.work_items", bid_id=bid_id))
 
 
-@work_item_blueprint.route("/add_work_item_to_cart", methods=["POST"])
+@work_item_blueprint.route("/add_work_item_to_cart/<bid_id>", methods=["POST"])
 @login_required
-def add_work_item_to_cart():
+def add_work_item_to_cart(bid_id):
     form = WorkItemCartForm(request.form)
     if form.validate_on_submit():
         selected_work_items_choices = [
@@ -64,67 +64,67 @@ def add_work_item_to_cart():
     elif form.is_submitted():
         log(log.WARNING, "The given data was invalid")
         # flash("The given data was invalid.", "danger")
-    return redirect(url_for("work_item.work_items"))
+    return redirect(url_for("work_item.work_items", bid_id=bid_id))
 
 
-@work_item_blueprint.route("/delete_work_item_from_cart/<item_id>", methods=["GET"])
+@work_item_blueprint.route("/delete_work_item_from_cart/<bid_id>/<item_id>", methods=["GET"])
 @login_required
-def delete_work_item_from_cart(item_id):
+def delete_work_item_from_cart(bid_id, item_id):
     item_id = str(item_id)
     selected_ids = session.get("SelectedWorkItemsDict", {})
     if item_id in selected_ids:
         session["DeletedWorkItem"] = selected_ids[item_id]
         del selected_ids[item_id]
         session["SelectedWorkItemsDict"] = selected_ids
-    return redirect(url_for("work_item.work_items"))
+    return redirect(url_for("work_item.work_items", bid_id=bid_id))
 
 
-@work_item_blueprint.route("/undo_work_item_from_cart/<item_id>", methods=["GET"])
+@work_item_blueprint.route("/undo_work_item_from_cart/<bid_id>/<item_id>", methods=["GET"])
 @login_required
-def undo_work_item_from_cart(item_id):
+def undo_work_item_from_cart(bid_id, item_id):
     item_id = str(item_id)
     deleted_work_item = session.get("DeletedWorkItem", {})
     selected_ids = session.get("SelectedWorkItemsDict", {})
     if deleted_work_item:
         selected_ids[item_id] = str(item_id)
         session["DeletedWorkItem"] = {}
-        return redirect(url_for("work_item.work_items"))
+        return redirect(url_for("work_item.work_items", bid_id=bid_id))
     session["SelectedWorkItemsDict"] = selected_ids
-    return redirect(url_for("work_item.work_items"))
+    return redirect(url_for("work_item.work_items", bid_id=bid_id))
 
 
 #  Group Manipulation
 
 
-@work_item_blueprint.route("/work_item_group", methods=["POST"])
+@work_item_blueprint.route("/work_item_group/<bid_id>", methods=["POST"])
 @login_required
-def work_item_group():
+def work_item_group(bid_id):
     form = WorkItemGroupForm(request.form)
     groups = session.get("GroupDict", {})
     group_name = form.name.data
     if group_name not in groups:
         groups[group_name] = []
         session["GroupDict"] = groups
-    return redirect(url_for("work_item.work_items"))
+    return redirect(url_for("work_item.work_items", bid_id=bid_id))
 
 
-@work_item_blueprint.route("/delete_work_item_from_group/<group_name>/<item_id>", methods=["GET"])
+@work_item_blueprint.route("/delete_work_item_from_group/<bid_id>/<group_name>/<work_item_id>", methods=["GET"])
 @login_required
-def delete_work_item_from_group(group_name, item_id):
+def delete_work_item_from_group(group_name, work_item_id, bid_id):
     groups = session.get("GroupDict", {})
     if group_name in groups:
-        if item_id in groups[group_name]:
-            groups[group_name].remove(item_id)
-            session["DeletedWorkGroupItem"] = {group_name: item_id}
+        if work_item_id in groups[group_name]:
+            groups[group_name].remove(work_item_id)
+            session["DeletedWorkGroupItem"] = {group_name: work_item_id}
         session["GroupDict"] = groups
     else:
         log(log.WARNING, "Not found group [%s]!", group_name)
-    return redirect(url_for("work_item.work_items"))
+    return redirect(url_for("work_item.work_items", bid_id=bid_id))
 
 
-@work_item_blueprint.route("/undo_work_item_from_group/<group_name>/<item_id>", methods=["GET"])
+@work_item_blueprint.route("/undo_work_item_from_group/<bid_id>/<group_name>/<item_id>", methods=["GET"])
 @login_required
-def undo_work_item_from_group(group_name, item_id):
+def undo_work_item_from_group(group_name, item_id, bid_id):
     item_id = str(item_id)
     deleted_work_item_group_id = session.get("DeletedWorkGroupItem", {})
     selected_ids = session.get("SelectedWorkItemsGroupDict", {})
@@ -132,22 +132,22 @@ def undo_work_item_from_group(group_name, item_id):
     if deleted_work_item_group_id:
         groups[group_name].append(item_id)
         session["DeletedWorkGroupItem"] = {}
-        return redirect(url_for("work_item.work_items"))
+        return redirect(url_for("work_item.work_items", bid_id=bid_id))
     session["SelectedWorkItemsGroupDict"] = selected_ids
-    return redirect(url_for("work_item.work_items"))
+    return redirect(url_for("work_item.work_items", bid_id=bid_id))
 
 
 @work_item_blueprint.route(
-    "/delete_group/<group_name>", methods=["POST"]
+    "/delete_group/<bid_id>/<group_name>", methods=["POST"]
 )
 @login_required
-def delete_group(group_name):
+def delete_group(bid_id, group_name):
     if group_name:
         groups = session.get("GroupDict", {})
         if str(group_name) in groups:
             del groups[group_name]
             session['GroupDict'] = groups
-    return redirect(url_for("work_item.work_items"))
+    return redirect(url_for("work_item.work_items", bid_id=bid_id))
 
 
 #  End Group Manipulation
@@ -157,10 +157,10 @@ def delete_group(group_name):
 
 
 @work_item_blueprint.route(
-    "/delete_work_item/<item_id>", methods=["POST"]
+    "/delete_work_item/<bid_id>/<item_id>", methods=["POST"]
 )
 @login_required
-def delete_work_item(item_id):
+def delete_work_item(bid_id, item_id):
     work_item = WorkItem.query.get(item_id)
     if work_item:
         selected = session.get("SelectedWorkItemsDict", {})
@@ -168,12 +168,12 @@ def delete_work_item(item_id):
             del selected[str(work_item.id)]
             session['SelectedWorkItemsDict'] = selected
         work_item.delete()
-    return redirect(url_for("work_item.work_items"))
+    return redirect(url_for("work_item.work_items", bid_id=bid_id))
 
 
-@work_item_blueprint.route("/edit_work_item/<item_id>", methods=["POST"])
+@work_item_blueprint.route("/edit_work_item/<bid_id>/<item_id>", methods=["POST"])
 @login_required
-def edit_work_item(item_id):
+def edit_work_item(bid_id, item_id):
     item_id = int(item_id)
     form = NewWorkItemForm(request.form)
     if form.validate_on_submit():
@@ -182,18 +182,18 @@ def edit_work_item(item_id):
             work_item.code = form.code.data
             work_item.name = form.name.data
             work_item.save()
-    return redirect(url_for("work_item.work_items"))
+    return redirect(url_for("work_item.work_items", bid_id=bid_id))
 
 
 #  End WorkItem Manipulation
 
 
-@work_item_blueprint.route("/work_items/", methods=["GET"])
+@work_item_blueprint.route("/work_items/<bid_id>", methods=["GET"])
 @login_required
-def work_items():
+def work_items(bid_id):
     form = NewWorkItemForm()
     form_group = WorkItemGroupForm()
-    bid = Bid.query.get(1)  ## !!!!!!!!!!!!!!!!! VERY BAD!!!!
+    bid_id = bid_id
     work_cart_form = WorkItemCartForm()
     selected_work_item_ids = session.get("SelectedWorkItemsDict", {})
     form.deleted_work_item_id = session.get("DeletedWorkItem", {})
@@ -216,7 +216,7 @@ def work_items():
         "work_items.html",
         form=form,
         form_group=form_group,
-        bid=bid,
+        bid_id=bid_id,
         work_cart_form=work_cart_form,
         str_function=str_function
     )
