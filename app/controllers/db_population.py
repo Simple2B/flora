8a9d336d51ff18f29a28c20a4dfce80d6281fc3c
dyman <380999257716.dyman@gmail.com>
2020-10-14 01:59:20 +0300

    try to repied code like made with Bid

--- app/controllers/db_population.py
+++ app/controllers/db_population.py
@@ -48,4 +48,16 @@ def populate_db_by_test_data():
         status=Bid.Status.b_draft
     ).save()
 
+    """ Clarification(
+        id=1,
+        note="Please note the following clarifications:",
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    ).save()
+
+    Exclusion(
+        id=1,
+        title="Unless expressly stated, the following exclusions apply",
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    ).save()"""
+
     db.session.commit()
