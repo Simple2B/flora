ae93f11c08ed5d58366e78aa99051ec115f541ed
Vitaly <dagger180@gmail.com>
2020-11-13 11:21:44 +0200

    add test-bids

--- app/controllers/db_population.py
+++ app/controllers/db_population.py
@@ -1,5 +1,5 @@
 from app import db
-from app.models import User, WorkItem, Exclusion, Clarification, Bid, WorkItemGroup
+from app.models import User, WorkItem, Exclusion, Clarification, WorkItemGroup
 from app.models import WorkItemLine, ClarificationLink, ExclusionLink, LinkWorkItem
 
 
@@ -37,32 +37,8 @@ def populate_db_by_test_data():
             )
         )
 
-    # bid save()
-    bid = Bid(
-        procore_bid_id=105,
-        title="bidding 5",
-        client="Procore (Test Companies)",
-        status=Bid.Status.b_draft
-    ).save()
-
-    for i in range(2):
-        bid = Bid(
-            procore_bid_id=106+i,
-            title="bidding i",
-            client="Procore (Test Companies)",
-            status=Bid.Status.c_submitted
-        ).save()
-
-    for i in range(2):
-        bid = Bid(
-            procore_bid_id=108+i,
-            title=f"bidding {3+i}",
-            client="Procore (Test Companies)",
-            status=Bid.Status.d_archived
-        ).save()
-    # end bid save()
     work_item = WorkItem.query.first()
-    link_wi = LinkWorkItem(bid_id=bid.id, work_item_id=work_item.id).save()
+    link_wi = LinkWorkItem(bid_id=1, work_item_id=work_item.id).save()
     WorkItemLine(
         note="Work Item line 1 note",
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit",
@@ -84,10 +60,10 @@ def populate_db_by_test_data():
     ######################################
     # Add Group1
     work_item_group = WorkItemGroup(
-        bid_id=bid.id,
+        bid_id=1,
         name="Group1"
     ).save()
-    link_wi = LinkWorkItem(bid_id=bid.id, work_item_id=work_item.id, work_item_group=work_item_group).save()
+    link_wi = LinkWorkItem(bid_id=1, work_item_id=work_item.id, work_item_group=work_item_group).save()
     WorkItemLine(
         note="Work Item line 1 in group1 ",
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit",
@@ -109,23 +85,23 @@ def populate_db_by_test_data():
 
     clarifications = Clarification.query.all()
     ClarificationLink(
-        bid_id=bid.id,
+        bid_id=1,
         clarification_id=clarifications[0].id
     ).save()
 
     ClarificationLink(
-        bid_id=bid.id,
+        bid_id=1,
         clarification_id=clarifications[1].id
     ).save()
 
     exclusion = Exclusion.query.all()
     ExclusionLink(
-        bid_id=bid.id,
+        bid_id=1,
         exclusion_id=exclusion[0].id
     ).save()
 
     ExclusionLink(
-        bid_id=bid.id,
+        bid_id=1,
         exclusion_id=exclusion[1].id
     ).save()
 
