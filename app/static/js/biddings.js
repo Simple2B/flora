7c7f9374d39a33a3d0bd1a00f35eaeeca5762785
Vitaly <dagger180@gmail.com>
2020-11-24 16:14:06 +0200

    add search-logic to biddings_page

--- app/static/js/biddings.js
+++ app/static/js/biddings.js
@@ -4,3 +4,16 @@ if ( biddings_.href == window.location.href ) {
   bid_href_id.classList.remove('menu__item');
   bid_href_id.classList.toggle('active-tab');
 };
+
+$(document).ready(function() {
+  $("#biddingsSearch").keyup(function() {
+  _this = this;
+
+  $.each($("#biddingsTableId tbody tr"), function() {
+      if($(this).text().toLowerCase().indexOf($(_this).val().toLowerCase()) === -1) {
+          $(this).hide();
+      } else {
+          $(this).show();
+      }});
+    });
+});
