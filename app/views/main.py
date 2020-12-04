44f1e4f270d0bee2e414cf1eae357ec03f59ddc5
Vitaly <dagger180@gmail.com>
2020-12-04 17:35:17 +0200

    remove 'team view' from 'main views'

--- app/views/main.py
+++ app/views/main.py
@@ -1,8 +1,5 @@
-from flask import render_template, Blueprint, redirect, url_for, request
+from flask import render_template, Blueprint, redirect, url_for
 from flask_login import login_required
-
-from app.forms import RegistrationForm
-from app.models import User
 
 
 main_blueprint = Blueprint("main", __name__)
@@ -12,14 +9,6 @@ main_blueprint = Blueprint("main", __name__)
 @login_required
 def index():
     return render_template("index.html")
-
-
-@main_blueprint.route("/team")
-@login_required
-def team():
-    form = RegistrationForm(request.form)
-    users = User.query.all()
-    return render_template("team.html", form=form, users=users)
 
 
 @main_blueprint.route("/resources")
