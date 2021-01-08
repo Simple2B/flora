472e153943d2d8764efb0b6616a031dfcaad44a1
Vitaly <dagger180@gmail.com>
2021-01-08 18:10:04 +0000

    init cron service

--- Dockerfile
+++ Dockerfile
@@ -21,5 +21,5 @@ RUN rm mycron
 # COPY . .
 
 EXPOSE 5000
-
+CMD service cron start
 CMD [ "flask", "run", "-h", "0.0.0.0" ]
--- update_bids_docker.sh
+++ update_bids_docker.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+FULL_PATH=$(realpath $0)
+# echo $FULL_PATH
+DIR_PATH=$(dirname $FULL_PATH)
+# echo $DIR_PATH
+cd $DIR_PATH
+
 flask update-bids 2>> /usr/src/app/update-bids.log
