392936d0e5852234a66d50e5330c2d406460a3f1
Vitaly <dagger180@gmail.com>
2021-01-12 09:47:40 +0000

    change base redirect to biddings_page

--- app/views/main.py
+++ app/views/main.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint, redirect, url_for
+from flask import Blueprint, redirect, url_for
 from flask_login import login_required
 
 
@@ -8,21 +8,10 @@ main_blueprint = Blueprint("main", __name__)
 @main_blueprint.route("/")
 @login_required
 def index():
-    return render_template("index.html")
+    return redirect(url_for("bidding.biddings"))
 
 
-@main_blueprint.route("/resources")
-@login_required
-def resources():
-    return render_template("header.html")
-
-
-@main_blueprint.route("/header")
-@login_required
-def header():
-    return render_template("header.html")
-
-
-@main_blueprint.route("/test")
-def test():
-    return redirect(url_for("bidding.bidding/#work-item-container"))
+# @main_blueprint.route("/resources")
+# @login_required
+# def resources():
+#     return render_template("header.html")
