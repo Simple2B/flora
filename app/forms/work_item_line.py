6853688f76bea2a68ae5de796ff1abc8549909fb
Vitaly <dagger180@gmail.com>
2021-01-16 10:40:58 +0000

    fix edit work_item_line

--- app/forms/work_item_line.py
+++ app/forms/work_item_line.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, FloatField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, InputRequired
 
 
 class WorkItemLineForm(FlaskForm):
     submit = SubmitField('Save')
     note = TextAreaField('Note')
     description = TextAreaField('Description')
-    price = FloatField('Price', validators=[DataRequired()])
+    price = FloatField('Price', validators=[InputRequired()])
     unit = StringField('Unit')
     quantity = FloatField('Quantity', validators=[DataRequired()])
     tbd = BooleanField('TDB')
