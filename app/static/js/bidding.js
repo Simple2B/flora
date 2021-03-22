947a700b43d1c7c3403e6f423b953f70e7eca652
Vitaly <dagger180@gmail.com>
2021-03-22 12:30:56 +0000

    fix bugs

--- app/static/js/bidding.js
+++ app/static/js/bidding.js
@@ -1,3 +1,15 @@
+const drawingLogCloseWrapper = document.getElementById('drawing_log_close_panel_id');
+const drawingLogClosePanel = document.getElementById('drawing_log_hidden_id');
+drawingLogCloseWrapper.addEventListener('click', (e) => {
+  e.preventDefault();
+  let changeDrawingLogImg = document.querySelector('#drawing_log_close_panel_id img').getAttribute('src');
+  drawingLogClosePanel.classList.toggle('hidden');
+  if (changeDrawingLogImg == "/static/images/up_direction_element.svg") {
+    document.querySelector('#drawing_log_close_panel_id img').setAttribute('src', "/static/images/direction_element_bottom.svg");
+  } else {
+    document.querySelector('#drawing_log_close_panel_id img').setAttribute('src', "/static/images/up_direction_element.svg");
+  };
+});
 
 $(document).ready(function() {
 
@@ -6,19 +18,6 @@ $(document).ready(function() {
     document.documentElement.scrollTop = Number(window.location.search.split("=").pop())
     window.history.replaceState({}, document.title, "/" + "bidding/" + `${bidID}`);
   }
-
-  const drawingLogCloseWrapper = document.getElementById('drawing_log_close_panel_id');
-  const drawingLogClosePanel = document.getElementById('drawing_log_hidden_id');
-  drawingLogCloseWrapper.addEventListener('click', (e) => {
-    e.preventDefault();
-    let changeDrawingLogImg = document.querySelector('#drawing_log_close_panel_id img').getAttribute('src');
-    drawingLogClosePanel.classList.toggle('hidden');
-    if (changeDrawingLogImg == "/static/images/up_direction_element.svg") {
-      document.querySelector('#drawing_log_close_panel_id img').setAttribute('src', "/static/images/direction_element_bottom.svg");
-    } else {
-      document.querySelector('#drawing_log_close_panel_id img').setAttribute('src', "/static/images/up_direction_element.svg");
-    };
-  });
 
   // Sidebar
   if (window.location.hash) {
